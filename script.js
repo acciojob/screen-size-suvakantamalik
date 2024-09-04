109172709328707bb8fc65806b1011960ea3ee5e
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-04 08:42:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateWindowSize() {
+	var h = window.innerHeight;
+	var w = window.innerWidth;
+
+	let h1 = document.getElementById("sizeInfo h1");
+	h1.innerText = `Width: ${w} and Height: ${h}`;
+}
+
+updateWindowSize();
+
+window.addEventListener("resize", updateWindowSize);
