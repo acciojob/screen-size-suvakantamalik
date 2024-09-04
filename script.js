2a97479b059637a0279437d93e557f58bd3bd421
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-04 18:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function updateSize() {
   var h = window.innerHeight;
   var w = window.innerWidth;
 
-  let h1 = document.querySelector(".sizeInfo h1");
+  let h1 = document.querySelector("h1");
   h1.textContent = `Width: ${w} and Height: ${h}`;
   console.log(h);
 }
-
-updateSize();
 
 window.addEventListener("resize", updateSize);
